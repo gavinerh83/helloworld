43e615f7545b295f71b0972526cc659e805accfa
ecnopronin <ecnopronin@yahoo.com>
2020-11-27 00:33:48 +0800

    commit for act 2 item 4 from newbranchERP redo

--- main.go
+++ main.go
@@ -6,4 +6,5 @@ func main() {
 	fmt.Println("Hello")
 	fmt.Println("Hello from branch")
 	fmt.Println("Hello from PonceER")
+	fmt.Println("Added another line for the 2nd time for Activity 2 item 3 from newbranchPER redo")
 }
