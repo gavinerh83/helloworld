65d33b726d98d1e688da6eee242f42abc2c3c604
ecnopronin <ecnopronin@yahoo.com>
2020-11-27 02:09:44 +0800

    commit for pull request activity

--- main.go
+++ main.go
@@ -6,4 +6,7 @@ func main() {
 	fmt.Println("Hello")
 	fmt.Println("Hello from branch")
 	fmt.Println("Hello from PonceER")
+	// modified the code to add another hello for activity 2 pull request
+	fmt.Println("Another hello")
+
 }
